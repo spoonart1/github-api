439dc59d13e35e589c0faf743b5377d04a722e03
spoonart1 <myspoonart@gmail.com>
2022-09-23 20:51:22 +0700

    init database

--- app/src/main/java/com/astro/test/lafran/database/AppDatabase.kt
+++ app/src/main/java/com/astro/test/lafran/database/AppDatabase.kt
@@ -1,4 +1,34 @@
 package com.astro.test.lafran.database
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java, "astro.db"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+
 }
