abbad53f380c5169fc939e1157963ecdfc4f5b2b
spoonart1 <myspoonart@gmail.com>
2022-09-23 21:52:09 +0700

    add user activity layout

---
+++ app/src/main/java/com/astro/test/lafran/feature/UserListActivity.kt
@@ -0,0 +1,18 @@
+package com.astro.test.lafran.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.astro.test.lafran.R
+
+class UserListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_list)
+
+        supportActionBar?.title = "Github User"
+        supportActionBar?.subtitle = "Astro Test"
+        supportActionBar?.elevation = 0f
+    }
+
+}
