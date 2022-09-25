73b895db1a6ec0884503ceb3082eb137139f717b
spoonart1 <myspoonart@gmail.com>
2022-09-25 11:31:18 +0700

    add filter unit test

--- app/src/test/java/com/astro/test/lafran/UserListViewModelTest.kt
+++ app/src/test/java/com/astro/test/lafran/UserListViewModelTest.kt
@@ -1,10 +1,11 @@
 package com.astro.test.lafran
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.astro.test.lafran.database.OrderBy
 import com.astro.test.lafran.feature.userlist.UserListUseCase
 import com.astro.test.lafran.feature.userlist.UserListViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -33,14 +34,23 @@ class UserListViewModelTest {
     }
 
     @Test
-    fun test_when_initializing_viewModel_Should_return_SinceValue_zero() {
+    fun test_initializing_viewModel_Should_return_SinceValue_zero() {
         assertEquals(0, viewModel.since.value)
     }
 
     @Test
-    fun test_when_setPage_SinceLiveDataValue_shouldReturnExactValue() {
+    fun test_setPage_SinceLiveDataValue_shouldReturnExactValue() {
         viewModel.setPage(1)
         assertEquals(1, viewModel.since.value)
+    }
+
+    @Test
+    fun test_setFilter_FilterLiveData_shouldReturnExactValue() {
+        viewModel.setFilter(OrderBy.DESC)
+
+        assertNotNull(viewModel.filter.value)
+        assertEquals(OrderBy.DESC, viewModel.filter.value!!.first)
+        assertNull(viewModel.filter.value!!.second)
     }
 
 }
